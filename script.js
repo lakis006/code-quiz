9983f854dfa3b9cfe9c626ea4ede3d78512ac1ab
Jsquad <lakis006@umn.edu>
2020-04-20 22:10:09 -0500

    added the html/css before doing any javascript

---
+++ script.js
@@ -0,0 +1,10 @@
+
+const startButton = document.querySelector("#questions-container");
+const btn1 = document.querySelector("#btn0");
+const btn2 = document.querySelector("#btn1");
+const btn3 = document.querySelector("#btn2");
+const btn4 = document.querySelector("#btn3");
+const btn4 = document.querySelector("#next");
+ 
+
+
