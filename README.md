8f6f6f3964de30815280e75e96a1818ca643b10e
Jsquad <lakis006@umn.edu>
2020-04-25 23:05:03 -0500

    added the the finishing touches by creating two more containers which included the input form, as well as the high score page. Also figured out the local storage logging

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # code-quiz
+
+The code quiz was mostly constructed using Javascript with a little JQuery sprinkled throughout the script. 
+
+The code allows the User to start the test and be taken to 4 question pages while keeping track of the wrong/right answers and punishes the user by subtracting 5 seconds from every wrong answer
+
+after the 30 second timed quiz is over, the user gets taken to a submit name page 
+
+after the user submits their name, they are taken to a scores page after saving their credentials into the local storage 
+
+link to deployed application ====> 
--- script.js
+++ script.js
@@ -1,17 +1,40 @@
 
-var container = document.querySelector("#container");
-var intCont = document.querySelector("#intro-container");
-var quizCont = document.querySelector("#quiz-container");
 
+
+var container = document.querySelector("#container");
+//intro page 
+var intCont = document.querySelector("#intro-container");
+var strbtn = document.querySelector("#st-btn");
+
+//quiz container things 
+var quizCont = document.querySelector("#quiz-container");
 var questionNumber = document.querySelector("#question-number");
+var q = document.querySelector("#question");
 var options = document.querySelector("#options");
 var btn0 = document.querySelector("#btn0");
 var btn1 = document.querySelector("#btn1");
 var btn2 = document.querySelector("#btn2");
 var btn3 = document.querySelector("#btn3");
-var resetBtn = document.querySelector("#restartquiz");
 var timer = document.querySelector("#timer");
-var strbtn = document.querySelector("#st-btn");
+
+var resetBtn = document.querySelector("#restartquiz");
+//input form things 
+var inputCont = document.getElementById("input-form-container");
+var scoreHolder = document.querySelector("#score");
+var inputField = document.querySelector("#input-field");
+var subtBtn = document.querySelector("#submit");
+
+//high scores things 
+var highScoreCont = document.querySelector("#highscore-container");
+var scoreL = document.querySelector("#score-list");
+var homeBtn = document.querySelector("#home-btn");
+var clearHighScore = document.querySelector("#clear-btn");
+
+
+//display homepage function
+displayHomePage()
+
+
 
 
 
@@ -25,27 +48,34 @@ var interval;
 //this will index our quesitons 
 var qIndex;
 
+//this will be how manny correct answers they have at a given time
+var usersScore;
 
-function startTest () {
+
+function startTest() {
     //hide all things that dont need to be there 
     //display the timer and start it 
     // start displaying questions 
 
 
     console.log("Starting Quiz");
-    time = 10;
+    time = 30;
 
     qIndex = 0;
 
+    usersScore = 0;
+
 
     //start the timer 
-    interval = startTime ();
+    interval = startTime();
 
     // were starting to display the question
-    startDisplayNextQuestion ();
+    startDisplayNextQuestion();
+    console.log("calling start Diplay next question from startTest function");
 
 }
- 
+
+
 
 function startTime() {
     console.log("startTime function ")
@@ -56,83 +86,199 @@ function runThisEverySecond() {
     //every second we want to decrease the time, check if timer has ran out
     time--;
     console.log(time);
-    //$("#timer").text(time);
+
     timer.innerHTML = time;
-    if (time == 0) {
+    if (time <= 0) {
         clearInterval(interval);
+        displayTheinputForm();
         console.log("Stopping timer");
     }
-    
+
 }
 
-function startDisplayNextQuestion () {
+function startDisplayNextQuestion() {
     // hide things and remove hide from other things 
     // then populate the page with a question 
 
     console.log("Start display fucntion got called")
     intCont.style.display = "none";
-    quizCont.style.display= "block";
+    quizCont.style.display = "block";
 
-    showQuestion(questionsList[qIndex]);
-    
-
-
+    //getRidOfBindings();
+    showQuestion(queList[qIndex]);
 
 }
 
 
-function displayHomePage(){
+function displayHomePage() {
+    intCont.style.display = "block";
     quizCont.style.display = "none";
+    inputCont.style.display = "none";
+    highScoreCont.style.display = "none";
+
     console.log("everything should be gone");
 }
 
-function displayHighScores(){
+function removeScores() {
+    while(scoreL.firstChild) {
+        scoreL.removeChild(scoreL.firstChild);
+    }
 }
 
+function displayHighScores() {
 
+    removeScores();
+
+    inputCont.style.display = "none";
+    //highScoreCont.style.diplay = "compact";
+    highScoreCont.style.display = "block";
+
+    console.log("diplay High Scores got called");
+
+    for (var i = 0; i < localStorage.length; i++) {
+        var person = localStorage.key(i);
+        var personScore = localStorage.getItem(localStorage.key(i));
+
+
+        var toShow = document.createElement('div');
+        
+        toShow.innerHTML = "Name: " + person + " Score is: " + personScore;
+
+        scoreL.appendChild(toShow);
+    }
+    homeBtn.addEventListener('click', displayHomePage);
+    clearHighScore.addEventListener('click', clearStorage);
+}
+
+function clearStorage() {
+    localStorage.clear();
+    displayHighScores();
+}
 //this will affect our HTML to print out the question and answers 
-function showQuestion( list  ){
-    $("#question-number").text("You are at quesiton " + (qIndex));
+//list is an object 
+function showQuestion(list) {
 
-    $("#question").text(list[0]);
+    questionNumber.innerHTML = qIndex;
 
-    $("#btn0").text(list[1][0]);
-    $("#btn1").text(list[1][2]);
-    $("#btn2").text(list[1][3]);
-    $("#btn3").text(list[1][1]);
-    
-    $("#btn0").on("click", () => {
-        qIndex++;
-        if (qIndex < questionsList.length) {startDisplayNextQuestion();}
-    })
-    $("#btn1").on("click", () => {
-        qIndex++;
-        if (qIndex < questionsList.length) {startDisplayNextQuestion();}
-    })
-    $("#btn2").on("click", () => {
-        qIndex++;
-        if (qIndex < questionsList.length) {startDisplayNextQuestion();}
-    })
-    $("#btn3").on("click", () => {
-        qIndex++;
-        if (qIndex < questionsList.length) {startDisplayNextQuestion();}
-    })
+    q.innerHTML = list.ask;
 
-    
+    btn0.innerHTML = list.answers[0].text;
+    btn0.dataset.ans = list.answers[0].correct;
 
+    btn1.innerHTML = list.answers[1].text;
+    btn1.dataset.ans = list.answers[1].correct;
+
+    btn2.innerHTML = list.answers[2].text;
+    btn2.dataset.ans = list.answers[2].correct;
+
+    btn3.innerHTML = list.answers[3].text;
+    btn3.dataset.ans = list.answers[3].correct;
+
+    btn0.addEventListener('click', ansClick);
+    btn1.addEventListener('click', ansClick);
+    btn2.addEventListener('click', ansClick);
+    btn3.addEventListener('click', ansClick);
+}
+
+function ansClick(buttonPressed) {
+
+    // console.log(buttonPressed.target.dataset.ans);
+    var theyCorrect = buttonPressed.target.dataset.ans;
+    console.log(theyCorrect);
+    if (theyCorrect == "true") {
+        usersScore = usersScore + 1;
+        console.log("Curent score is " + usersScore);
+    } else {
+        time = time - 5;
+    }
+
+    // check their answer and dicrease the timer i wilsh 
+    // increment the the score if they answered corerctly 
+    qIndex++;
+    if (qIndex < queList.length) {
+        startDisplayNextQuestion();
+        console.log("calling start Diplay next question from anwer click");
+    }
+    else {
+        console.log("DONE WITH THE QUIZ!!! NICE JOB!!");
+        clearInterval(interval);
+        displayTheinputForm();
+    }
+}
+
+function displayTheinputForm() {
+    console.log("Still under construction!");
+    //quizCont.style.diplay = "none";
+    $(quizCont).hide();
+    inputCont.style.display = "block";
+    scoreHolder.innerHTML = usersScore;
+
+
+    subtBtn.addEventListener('click', () => {
+        var n = inputField.value;
+        saveScore(n, usersScore);
+        displayHighScores();
+    }
+    );
 
 
 }
 
+function saveScore(n, s) {
+    localStorage.setItem(n, s);
+    console.log("Storing this name: " + n + " and this value: " + s);
+}
 
-var questionsList = [ ["WHAT WHAT?" , [ 1 ,"in da butt" ,2 ,7] ], ["what is TWO" , [ 1 ,3 ,2 ,7] ] , ["what is 1 +1?" , [ 1 ,3 ,2 ,7] ] , ["what is 17 + 33?" , [ 50  ,44  ,06 ,70 ] ] , ["what is 1 + 4?" , [ 1 ,3 ,2 ,7] ] ];
 
 
-// var theNumberThree = questionsList[0][1][1];
-// var theNUmberSeventy = questionsList[1][1][3];
-// console.log(theNUmberSeventy);
+var queList = [
+    {
+        ask: "what says woof?",
+
+        answers: [
+            { text: "dog", correct: true },
+            { text: "frog", correct: false },
+            { text: "cat", correct: false },
+            { text: "apple", correct: false }
+        ]
+    },
+    {
+        ask: "what is 100 + 100?",
+
+        answers: [
+            { text: 78, correct: false },
+            { text: 309, correct: false },
+            { text: 12, correct: false },
+            { text: 200, correct: true }
+        ]
+    }
+    ,
+    {
+        ask: "What is Love?",
+
+        answers: [
+            { text: "Don't Ask", correct: false },
+            { text: "Love Don't Live Here", correct: false },
+            { text: "Baby Don't Hurt Me", correct: true },
+            { text: "Hopeless Romantic", correct: false }
+        ]
+    }
+    ,
+    {
+        ask: "what says Ribbet?",
+
+        answers: [
+            { text: "dog", correct: false },
+            { text: "frog", correct: true },
+            { text: "cat", correct: false },
+            { text: "apple", correct: false }
+        ]
+    }
+]
+
 
 strbtn.addEventListener("click", startTest);
 
-//as soon as the html finishes loading => execute the function 
-$(document).ready(displayHomePage());
+displayHomePage();
+
+
